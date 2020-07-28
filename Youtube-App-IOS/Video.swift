40072872703fbfe4f9881f68490c4376cf81ff3c
Kapil_bhattarai <kapilbhattarai5@gmail.com>
2020-07-28 11:35:55 +0545

    Add : api key and model added

---
+++ Youtube-App-IOS/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  Youtube-App-IOS
+//
+//  Created by Kapil Bhattarai on 7/28/20.
+//  Copyright © 2020 Kapil Bhattarai. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
